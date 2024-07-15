1307a3afe829e5a7d88c0660b189948ffbe1b07d
AurRazex <AlekSevrus@gmail.com>
2024-07-15 21:46:16 +0300

    Ver.6 (Project L 5)
    Проект с Д\З 5 внутри проекта Д\З 1-3

---
+++ AutoTestLesson5/src/main/java/HWs/Main.java
@@ -0,0 +1,7 @@
+package HWs;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
---
+++ AutoTestLesson5/src/main/java/Lesson5/weather/dayWeatherAlarms/Hhh.java
@@ -0,0 +1,4 @@
+package Lesson5.weather.dayWeatherAlarms;
+
+public class Hhh {
+}
